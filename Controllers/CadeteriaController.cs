50b3095745b31667c54df1f0c6a3c11e5e7a27e3
Tadeo Munoz <tadeomunoz09@hotmail.com>
2025-09-20 00:18:32 -0300

    agregue las acciones crearPedido, agregarPedido, asignarPedido, cambairEstadoPedido, cambiarCadetePedido

--- Controllers/CadeteriaController.cs
+++ Controllers/CadeteriaController.cs
@@ -45,4 +45,53 @@ public class CadeteriaController : ControllerBase
 
         return Ok(info);
     }
+
+
+    //Creo esta clase auxiliar para no exponer toda mi clase pedido en el front
+    //consultar en clase !! donde ubicarla, si es buena practica o no.
+    public class CrearPedidoDto
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public string Referencia { get; set; }
+        public string Obs { get; set; }
+    }
+    //------------------------------
+
+    [HttpPost("pedidos")]
+    public IActionResult AgregarPedido([FromBody] CrearPedidoDto dto)
+    {
+        if (dto == null || string.IsNullOrWhiteSpace(dto.Nombre)) return BadRequest();
+
+        bool ok = _service.AltaPedido(dto.Nombre, dto.Direccion, dto.Telefono, dto.Referencia, dto.Obs);
+
+        return ok ? Created("", null) : BadRequest();
+    }
+
+    [HttpPut("pedidos/{idPedido}/asignar/{idCadete}")]
+    public IActionResult AsignarPedido(int idPedido, int idCadete)
+    {
+        var ok = _service.AsignarPedido(idPedido, idCadete);
+
+        return ok ? NoContent() : NotFound();
+    }
+
+    [HttpPut("pedidos/{idPedido}/estado/{nuevoEstado}")]
+    public IActionResult CambiarEstadoPedido(int idPedido, int nuevoEstado)
+    {
+        var ok = _service.CambiarEstadoPedido(idPedido, nuevoEstado);
+
+        return ok ? NoContent() : BadRequest();
+    }
+
+    [HttpPut("pedidos/{idPedido}/cambiarCadete/{idNuevoCadete}")]
+    public IActionResult CambiarCadetePedido(int idPedido, int idNuevoCadete)
+    {
+        var ok = _service.AsignarPedido(idPedido, idNuevoCadete);
+
+        return ok ? NoContent() : NotFound();
+    }
+
+
 }
